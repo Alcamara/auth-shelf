2b8153052eca34be7a3ac66a24fe826c432b9b1e
Al Camara <Alhousseinec@gmail.com>
2022-06-30 11:50:31 -0500

    add useEffect to shelfPage.js

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 
 function ShelfPage() {
+
+  const dispatch = useDispatch()
+  // const shelf = useSelector(store => s )
+
+  useEffect(()=>{
+    dispatch({
+      type: 'FETCH_SHELF'
+    })
+  })
+
+
   return (
     <div className="container">
       <h2>Shelf</h2>
