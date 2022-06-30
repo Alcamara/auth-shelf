8914ee50c49a630d2b946315603feb4f2bf435e2
Al Camara <Alhousseinec@gmail.com>
2022-06-30 11:40:26 -0500

    set up shelf get request

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,17 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+   // For testing only, can be removed
+   const query = `
+    SELECT * FROM item;
+   `
+   pool.query(query)
+    .then((results)=>{
+        res.send(results.rows)
+    })
+    .catch((err)=>{
+      console.error(`Query failed: ${err}`);
+    })
 });
 
 /**
