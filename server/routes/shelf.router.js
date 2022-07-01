36e129b8a1c1af2dd11ce3f2255b7bfd55dded3d
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-01 09:44:17 -0500

    POST saga and router completed

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
 /**
  * Get all of the items on the shelf
@@ -22,8 +23,27 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   // endpoint functionality
+  console.log('in POST', req.body);
+
+  const sqlQuery = `
+    INSERT INTO item (description, image_url, user_id)
+    VALUES ($1, $2, $3)`;
+  const sqlParams = [
+    req.body.description,
+    req.body.image_url,
+    req.user.id
+  ];
+  pool.query(sqlQuery, sqlParams)
+  .then((results) => {
+    console.log('POST is sending', results.rows);
+    res.send(results.rows);
+  })
+  .catch((err) => {
+    console.log('error in post router', err);
+    res.sendStatus(500);
+  })
 });
 
 /**
